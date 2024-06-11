74a2596326d3de047833b95c104a870f3f295205
ZWEIGST <watzwer@gmail.com>
2024-06-11 23:06:19 +0300

    decomposition

---
+++ src/Components/postList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="post" v-for="post in posts" :key="post.id">
+          <div><strong>Название</strong> {{ post.title }}</div>
+          <div><strong>Описание</strong> {{ post.body }}</div>
+       </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.post {
+  padding: 15px 0px 15px 0px;
+  border: 1px solid rgb(184, 132, 184);
+  margin-top: 15px;
+}
+</style>
